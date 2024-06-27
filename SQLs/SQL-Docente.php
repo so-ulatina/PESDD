671a0acf21ff5921deeeb48e4ea6cb1deeca46ac
LuiscaGonzalez16 <luis.gonzalez16@ulatina.net>
2024-06-27 15:00:24 -0600

    Update SQL-Docente.php

--- SQLs/SQL-Docente.php
+++ SQLs/SQL-Docente.php
@@ -1,24 +1,40 @@
 <?php
 session_start();
-require_once "../ConexionBaseDatos/Conexion.php"; 
+require_once "../ConexionBaseDatos/Conexion.php";
 
 
 
-if (isset($_POST['submitf'])){
+if (isset($_POST['submitf'])) {
 
     $find1 = $_POST['find1'];
-   
+    $find2 = $_POST['find2'];
 
 
-    if($find1){
+    if ((!$find1) && ($find2)) {
 
-        $_SESSION['sql1'] = "SELECT * FROM Docente  WHERE Nombre LIKE '%$find1%'";
+        $_SESSION['sql1'] = "SELECT * FROM Docente WHERE Estado = '$find2'";
         header("location: ../Paginas/AdministrarDocente.php");
     }
 
-   
+    if (($find1) && (!$find2)) {
 
-}     
+
+        $_SESSION['sql1'] = "SELECT * FROM Docente WHERE Nombre LIKE '%$find1%'";
+        header("location: ../Paginas/AdministrarDocente.php");
+    }
+
+
+    if (($find1) && ($find2)) {
+        $_SESSION['sql1'] = "SELECT * FROM Docente WHERE Estado = '$find2' and Nombre LIKE '%$find1%' ";
+        header("location: ../Paginas/AdministrarDocente.php");
+    }
+
+    if ((!$find1) && (!$find2)) {
+        header("location: ../Paginas/AdministrarDocente.php");
+    }
+
+}
+
 
 
 if (isset($_POST['submitundo'])){
@@ -47,11 +63,11 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
         $Nombre = $_POST['Nombre'];
         $Apellido1 = $_POST['Apellido1'];
         $Apellido2 = $_POST['Apellido2'];
-        $Nivel_Academico = $_POST['Nivel_Academico'];
+        $Nivel_Academico = trim(explode("-", $_POST['Id_NivelAcademico'])[0]);
         $Correo= $_POST['Correo'];
         $Celular = $_POST['Celular'];
         $Fecha_Nacimiento = $_POST['Fecha_Nacimiento'];
-
+        $Estado = $_POST['Estado'];
 
 
     }
@@ -66,11 +82,11 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
         $Nombre = $_POST['Nombree'];
         $Apellido1 = $_POST['Apellido1e'];
         $Apellido2 = $_POST['Apellido2e'];
-        $Nivel_Academico = $_POST['Nivel_Academicoe'];
+        $Nivel_Academico = trim(explode("-", $_POST['Id_NivelAcademicoe'])[0]);
         $Correo = $_POST['Correoe'];
         $Celular = $_POST['Celulare'];
         $Fecha_Nacimiento = $_POST['Fecha_Nacimientoe'];
-
+        $Estado = $_POST['Estadoe'];
 
     }
 
@@ -88,7 +104,7 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
         $Correo = $_POST['Correoel'];
         $Celular = $_POST['Celularel'];
         $Fecha_Nacimiento = $_POST['Fecha_Nacimientoel'];
-
+        $Estado = $_POST['Estadoel'];
 
     }
 
@@ -96,20 +112,17 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
     $sql = "SELECT * FROM Docente WHERE Id_cedula = '$Id_cedula'"; //Filtro con correo en la base de datos
     $resultado = mysqli_query($conn, $sql);
+    $docente = mysqli_fetch_array($resultado, MYSQLI_ASSOC);
     $rowCount = mysqli_num_rows($resultado); //Obtener las lineas obtenidas de la base de datos con el resultado obtenido despues del filtro
 
 
-    $sql2 = "SELECT * FROM Docente WHERE Correo = '$Correo'"; //Filtro con correo en la base de datos
-    $resultado2 = mysqli_query($conn, $sql2);
-    $rowCount2 = mysqli_num_rows($resultado2); //Obtener las lineas obtenidas de la base de datos con el resultado obtenido despues del filtro
-
-
+  
 
 
     //Validacion que la lineas obtenidas sean mayor a 0,lo cual significa que si se encontro una linea con el mismo correo 
 
     if ($rowCount > 0) {
-        array_push($errors, 'Ya existe un registro con la informacion igresada');
+        array_push($errors, 'Ya existe un registro con la informaci&oacuten igresada');
     }
 
 
@@ -121,7 +134,7 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
 
 
-            $sql = "INSERT INTO Docente  (Id_cedula,Id_Docente,Id_carrera,Nombre,Apellido1,Apellido2,Nivel_Academico,Correo,Celular,Fecha_Nacimiento) VALUES (?,?,?,?,?,?,?,?,?,?) ";
+            $sql = "INSERT INTO Docente  (Id_cedula,Id_Docente,Id_carrera,Nombre,Apellido1,Apellido2,Id_NivelAcademico,Correo,Celular,Fecha_Nacimiento,Estado ) VALUES (?,?,?,?,?,?,?,?,?,?,?) ";
             $stmt = mysqli_stmt_init($conn);
             $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
 
@@ -130,7 +143,7 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
 
 
-                mysqli_stmt_bind_param($stmt,"ssssssssss", $Id_cedula, $Id_Docente, $Id_carrera, $Nombre, $Apellido1, $Apellido2, $Nivel_Academico, $Correo, $Celular, $Fecha_Nacimiento);
+                mysqli_stmt_bind_param($stmt,"sssssssssss", $Id_cedula, $Id_Docente, $Id_carrera, $Nombre, $Apellido1, $Apellido2, $Nivel_Academico, $Correo, $Celular, $Fecha_Nacimiento, $Estado);
                 mysqli_stmt_execute($stmt);
 
 
@@ -144,11 +157,20 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
 
             if ($stmt->errno == 0) {
-                $_SESSION['resultado'] = "<div class='mensaje-sql-exitoso'><h1> Acción realizada stisfactoriamente</h1></div>";
+                $_SESSION['resultado'] = "<div class='mensaje-sql-exitoso'><h1> Acci&oacuten realizada stisfactoriamente</h1></div>";
                 header("location: ../Paginas/AdministrarDocente.php");
             } else {
-                $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Se genero un error al realizar la acción,intente de nuevo</h1></div>";
-                header("location: ../Paginas/AdministrarDocente.php");
+                if ($stmt->errno == 1062) {
+                    $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Ya existe un registro con la informaci&oacuten ingresada</h1></div>";
+                    header("location: ../Paginas/AdministrarDocente.php");
+
+                } else {
+
+
+                    $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Se genero un error al realizar la acci&oacuten,intente de nuevo</h1></div>";
+                    header("location: ../Paginas/AdministrarDocente.php");
+
+                }
             }
 
         } // fin agregar
@@ -163,7 +185,7 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
         if ($accion == 'Agregar'){
 
-            $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Ya existe un registro con la informacion ingresada</h1></div>";
+            $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Ya existe un registro con la informaci&oacuten ingresada</h1></div>";
             header("location: ../Paginas/AdministrarDocente.php");
         }
 
@@ -172,9 +194,16 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
         if ($accion == 'Editar') {
 
+            if ($Correo == $docente["Correo"]){
+                $sql = " UPDATE `Docente` SET `Id_cedula` = '$Id_cedula',`Id_Docente` = '$Id_Docente',`Id_carrera` = '$Id_carrera',`Nombre` = '$Nombre',`Apellido1` = '$Apellido1',`Apellido2` = '$Apellido2',`Id_NivelAcademico` = '$Nivel_Academico',`Celular` = '$Celular',`Fecha_Nacimiento` = '$Fecha_Nacimiento' , `Estado` = '$Estado' WHERE `Docente`.`Id_cedula` = $Id_cedula";
 
-            $sql = " UPDATE `Docente` SET `Id_cedula` = '$Id_cedula',`Id_Docente` = '$Id_Docente',`Id_carrera` = '$Id_carrera',`Nombre` = '$Nombre',`Apellido1` = '$Apellido1',`Apellido2` = '$Apellido2',`Nivel_Academico` = '$Nivel_Academico',`Correo` = '$Correo',`Celular` = '$Celular',`Fecha_Nacimiento` = '$Fecha_Nacimiento'  WHERE `Docente`.`Id_cedula` = $Id_cedula";
+            }else {
+                $sql = " UPDATE `Docente` SET `Id_cedula` = '$Id_cedula',`Id_Docente` = '$Id_Docente',`Id_carrera` = '$Id_carrera',`Nombre` = '$Nombre',`Apellido1` = '$Apellido1',`Apellido2` = '$Apellido2',`Id_NivelAcademico` = '$Nivel_Academico',`Correo` = '$Correo',`Celular` = '$Celular',`Fecha_Nacimiento` = '$Fecha_Nacimiento' , `Estado` = '$Estado' WHERE `Docente`.`Id_cedula` = $Id_cedula";
 
+            }
+            
+            
+            
 
             $stmt = mysqli_stmt_init($conn);
             $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
@@ -198,73 +227,72 @@ if (  (isset($_POST['submita'])) || (isset($_POST['submited'])) || (isset($_POST
 
 
             if ($stmt->errno == 0) {
-                $_SESSION['resultado'] = "<div class='mensaje-sql-exitoso'><h1> Acción realizada stisfactoriamente</h1></div>";
+                $_SESSION['resultado'] = "<div class='mensaje-sql-exitoso'><h1> Acci&oacuten realizada stisfactoriamente</h1></div>";
                 header("location: ../Paginas/AdministrarDocente.php");
             } else {
-                $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Se genero un error al realizar la acción,intente de nuevo</h1></div>";  //Error al realizar el sql
-                header("location: ../Paginas/AdministrarDocente.php");
+                if ($stmt->errno == 1062) {
+                    $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Ya existe un registro con la informaci&oacuten ingresada</h1></div>";
+                    header("location: ../Paginas/AdministrarDocente.php");
+
+                } else {
+
+
+                    $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Se genero un error al realizar la acci&oacuten,intente de nuevo</h1></div>";
+                    header("location: ../Paginas/AdministrarDocente.php");
+
+                }
             }
 
         
         }// Fin Editar
 
 
-     
+
 
 
         if ($accion == 'Eliminar') {
 
-            
-           
-         
-            $sql = "DELETE FROM Docente WHERE Id_cedula = $Id_cedula";
-            $stmt = mysqli_stmt_init($conn);
-            $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+
+            //Validacion si hay dependencia 
+
+            $sql1 = "SELECT * FROM Cursos_Matriculados WHERE Id_cedula= '$Id_cedula'"; //Filtro con correo en la base de datos
+            $resultado = mysqli_query($conn, $sql1);
+            $rowCount = mysqli_num_rows($resultado); //Obtener las lineas obtenidas de la base de datos con el resultado obtenido despues del filtro
 
 
-            if ($prepareStmt) {
-
-                mysqli_stmt_execute($stmt);
-
-
-
-            } // fin de if ($prepareStmt) 
-            else { // else de if ($prepareStmt)
-
-                echo mysqli_error($conn);
-                die('Algo salio mal');
-
-            } //fin else de if ($prepareStmt)
-
-
-
-
-
-            if ($stmt->errno == 0) {
-
-
-                $_SESSION['resultado'] = "<div class='mensaje-sql-exitoso'><h1> Accion realizada stisfactoriamente</h1></div>";
+            if ($rowCount > 0) {
+                $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> No se puede borrar la entrada debido a que tiene relaci&oacuten a otro registro</h1></div>";
                 header("location: ../Paginas/AdministrarDocente.php");
-
-
             } else {
 
-                if ($stmt->errno == "1451") {
-                    $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> No se puede borrar la entrada debido a que tiene relacion a otro registro</h1></div>";
+
+
+                $sql = "DELETE FROM Docente WHERE Id_cedula = $Id_cedula";
+                $stmt = mysqli_stmt_init($conn);
+                $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+
+                if ($prepareStmt) {
+
+                    mysqli_stmt_execute($stmt);
+
+                } // fin de if ($prepareStmt) 
+                else { // else de if ($prepareStmt)
+
+                    echo mysqli_error($conn);
+                    die('Algo salio mal');
+
+                } //fin else de if ($prepareStmt)
+
+
+                if ($stmt->errno == 0) {
+                    $_SESSION['resultado'] = "<div class='mensaje-sql-exitoso'><h1> Informacion Eliminada stisfactoriamente</h1></div>";
                     header("location: ../Paginas/AdministrarDocente.php");
-                } else {
-
-
-                    $_SESSION['resultado'] = "<div class='mensaje-sql-error'><h1> Se genero un error al ingresar la informacion,intente de nuevo</h1></div>";
-                    header("location: ../Paginas/AdministrarDocente.php");
-
                 }
+
+
+
+
             }
-
-
-
-
-
 
 
 
