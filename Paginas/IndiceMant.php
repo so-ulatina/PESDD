995b42327ff1bbe1eb22530e126ed04981b64017
LuiscaGonzalez16 <luis.gonzalez16@ulatina.net>
2024-06-27 14:58:12 -0600

    Update IndiceMant.php

--- Paginas/IndiceMant.php
+++ Paginas/IndiceMant.php
@@ -2,7 +2,9 @@
 
 <?php
 use MongoDB\Driver\Session;
+
 include ('../SecureLogin.php');
+$_SESSION['sql1'] = "";
 
 
 $_SESSION['Nombre'];
@@ -17,227 +19,93 @@ $_SESSION['Nombre'];
       <!-- Declaracion variables-->
     <?php
     $titulo = "Bienvenid@s";
-    
-        
+
+
     ?>
 
     <!-- Librearias y links-->
     
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="../CSS/Style-General.css" rel="stylesheet"/>
-
-    
-
 
 </head>
 
 <body >
-
-    
-
-
-    <div class="container">
-    
-     <!-- ----------------------------------Inicio de aside , barra iquierda ---------------------------------- -->
-    <div class="sidebar-global">
-        
-   
-        <aside>
-            
-            <div class="top">
-
-                  <div class="logo">
-                  <img src="../Imagenes/Logo.png" />
-                  </div>
-
-             </div>
-
-
-            <div class="sidebar">
-                
-                   
-                <div id="Administrar" class="item">
-
-                <a href="#" >
-                
-                   <h3> <span class="material-symbols-outlined">admin_panel_settings</span>Administrar <span class="material-symbols-outlined">keyboard_arrow_down</span></h3>
-                    
-                
-                </a>
-                
-                    <div class="sub-menu">
-                        <a class="item" href="#"> Docentes</a>
-                        <a class="item" href="#"> Facultades</a>
-                    </div>
-
-                 </div>   
-                    
-
-                
-
-                 <div id="Reportes" class="item">
-
-                <a href="#" >
-                
-                   <h3> <span class="material-symbols-outlined">summarize</span>Reportes <span class="material-symbols-outlined">keyboard_arrow_down</span> </h3>
-                    
-                
-                </a>
-                
-                    <div class="sub-menu">
+        <header>
+                <nav >
+               
+                    <ul>
+               
+                  <li class="logo"><img src="../Imagenes/Logo.jpg" /></li>
                   
-                        <a class="item" href="../Paginas/AdministrarModalidad.php"> Modalidades</a>
-                        <a class="item" href="../Paginas/AdministrarFacultad.php"> Facultades</a>
-                        <a class="item" href="../Paginas/AdministrarAdministrador.php"> Administrador</a>
-                        <a class="item" href="../Paginas/AdministrarCarrera.php"> Carrera</a>
-                        <a class="item" href="../Paginas/AdministrarCertificado.php"> Certificado</a>
-                        <a class="item" href="../Paginas/AdministrarDocente.php"> Docente</a>
-                        <a class="item" href="../Paginas/AdministrarSede.php"> Sede</a>
-                        <a class="item" href="../Paginas/AdministrarCategoria.php"> Categoria</a>
-                        <a class="item" href="../Paginas/AdministrarCuatrimestre.php"> Cuatrimestre</a>
-                        <a class="item" href="../Paginas/AdministrarCurso.php"> Curso</a>
-                        <a class="item" href="../Paginas/AdministrarSede.php"> Cursos Matriculados</a>
-                        <a class="item" href="../Paginas/AdministrarFacultad.php"> Facultad</a>
-                    </div>
+                    <li>
+                     <a href="#"><h3>Administrar</h3> </a> 
+                    <ul class="dropdown">
+                        <li><a  href="../Paginas/AdministrarModalidad.php"><h3> Modalidades</h3></a></li>
+                       <li> <a  href="../Paginas/AdministrarFacultad.php"><h3> Facultades</h3></a></li>
+                       <li> <a  href="../Paginas/AdministrarAdministrador.php"> <h3>Administrador</h3></a></li>
+                       <li> <a  href="../Paginas/AdministrarCarrera.php"> <h3>Carrera</h3></a></li>
+                      <li>  <a  href="../Paginas/AdministrarCertificado.php"> <h3>Certificado</h3></a></li>
+                       <li> <a  href="../Paginas/AdministrarDocente.php"> <h3>Docente</h3></a></li>
+                      <li>  <a  href="../Paginas/AdministrarSede.php"> <h3>Sede</h3></a></li>
+                      <li>  <a  href="../Paginas/AdministrarCategoria.php"> <h3>Categoria</h3></a></li>
+                      <li>  <a  href="../Paginas/AdministrarCuatrimestre.php"><h3> Cuatrimestre</h3></a></li>
+                      <li>  <a  href="../Paginas/AdministrarCurso.php"> <h3>Curso</h3></a></li>
+                       <li> <a  href="../Paginas/AdministrarCursosMatriculados.php"> <h3>Cursos Matriculados</h3></a></li>
+                      <li><a  href="../Paginas/AdministrarNivelAcademico.php"> <h3>Nivel Acad&eacute;mico</h3></a></li>
+                    </ul>
+                    </li>
+                    <li>
+                     <a href="#"><h3>Reportes</h3> </a> 
+                    <ul class="dropdown">
+                        <li><a class="item" href="../Reportes/Reporte-CursosMatriculadosDocentes.php"><h3> Cursos por Docente</h3></a></li>
+                    </ul>
+                    </li>
 
-                 </div>  
-
-
-
-                <a  href="#" >
+                    <li class="logout"> 
                 
-                   <h3> <span class="material-symbols-outlined">info</span>Acerca</h3>
-                    
-                
-                </a>
+                   <a class="last-link" href="../SecureLogOut.php" ><h3> <span class="material-symbols-outlined">logout</span>Cerrar Sesi&oacuten</h3></a>
+                    </li>
+                </ul>
+                </nav>
+            </header>    
+                  
 
-                <a  href="#" >
-                
-                   <h3> <span class="material-symbols-outlined">contacts</span> Contacto</h3>
-                   
-                
-                </a>
-
-
-                 
-                 <a class="last-link" href="../SecureLogOut.php" >
-                
-                   <h3> <span class="material-symbols-outlined">logout</span>Cerrar Sesion</h3>
-                    
-                
-                </a>
-
-
-
-            </div>
-
-        </aside>
     
-   
-    </div>
-     <!-- ----------------------------------Fin de aside , barra iquierda ---------------------------------- -->
-
-          <!-- ----------------------------------Inicio de main , centro pagina---------------------------------- -->
-
-        
-    <main>
-    
-
-
-        
-            <div class="date">
-                <p id="date-container"></p>
-            </div>
-
-
-
-
-
-        <div class="tabla-centro">
-
-
-
-            <h2>  <?php echo $titulo; ?> </h2>
-
-
-
-
-
-            <div class="Centro">
-
-
-   
-             </div>
-
-          
-      
-        
-      
-         
-             
-
-</div>
-
-</main>
-
-
-     <!-- ----------------------------------Fin de main , centro pagina---------------------------------- -->
-
-
-
-        <!-- ----------------------------------Inicio Top derecha---------------------------------- -->
-
-        <div class="right">
-
-
-
-            <div class="top">
-
-                <div class="perfil">
-                    
-                    <div class="info">
-                        <p> Hola , <b> <?php echo $_SESSION['Nombre']; ?> </b></p>
-                        
-                    </div>
-                
-
-                
-                <div class="perfil-foto">
-                    <span class="material-symbols-outlined">account_circle</span>
-                </div>
-                
-                </div>
-
-
-            </div>
-
-
-
-
-
-
+         <div class="indice">
+        <div class="Indiceleft">
 
         </div>
 
+        
+        <div class="Indiceright">
 
-        <!-- ----------------------------------Fin Top derecha---------------------------------- -->
-
-
-
-
-    </div> <!-- Fin div Container-->
+          </div>
+            </div>
 
 
-   
+
+
+       
+
+
 
     <!-- ------------ JavaScripts -----------------------  -->
-
+<div class="separator"></div>
    
+<footer>
 
-    
- 
- 
+
+              <div class="icons-sociales">
+                <a class="a-iconos" href="#"> <i class="bi bi-facebook"></i></a>
+                <a class="a-iconos" href="#"> <i class="bi bi-youtube"></i></a>
+                <a class="a-iconos" href="#"> <i class="bi bi-twitter"></i></a>  
+                <a class="a-iconos" href="#"> <i class="bi bi-instagram"></i></a>  
+                <a class="a-iconos" href="#"> <i class="bi bi-linkedin"></i></a> 
+                </div>
+
+</footer>
 
 </body>
 
