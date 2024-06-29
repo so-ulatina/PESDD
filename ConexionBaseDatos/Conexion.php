79e0d215f53779c74ca9738bf0872503ba1bd023
LuiscaGonzalez16 <luis.gonzalez16@ulatina.net>
2024-06-29 16:37:03 -0600

    Update Conexion.php

--- ConexionBaseDatos/Conexion.php
+++ ConexionBaseDatos/Conexion.php
@@ -1,8 +1,8 @@
 <?php
 
 $hostNombre="localhost";
-$dbUsuario ="root";
-$dbPassword ="";
+$dbUsuario ="Admin_Desarrollo_Docente";
+$dbPassword ="Desarrollo_2024";
 $dbName= "Desarrollo_Docentes";
 $conn = new mysqli($hostNombre,$dbUsuario,$dbPassword,$dbName);
 
